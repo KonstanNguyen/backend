ac8b08fa03779f2b62ccc8a5c48e9dd31acd6c77
Nguyen Khang <nguyenkhangnt2412@gmail.com>
2024-11-28 04:08:12 +0700

    add_document_service

--- src/main/java/com/systems/backend/controller/DocumentController.java
+++ src/main/java/com/systems/backend/controller/DocumentController.java
@@ -1,7 +1,14 @@
 package com.systems.backend.controller;
 
+import com.systems.backend.service.DocumentService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class DocumentController {
+    @Autowired
+    private DocumentService documentService;
 }
