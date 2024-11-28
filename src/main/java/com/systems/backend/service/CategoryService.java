a9ac4f6422a2e76e2e0691e4db6a081050f16b97
Nguyen Khang <nguyenkhangnt2412@gmail.com>
2024-11-28 12:35:07 +0700

    add_category_service

--- src/main/java/com/systems/backend/service/CategoryService.java
+++ src/main/java/com/systems/backend/service/CategoryService.java
@@ -1,7 +1,17 @@
 package com.systems.backend.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.systems.backend.model.Category;
 
 @Service
 public interface CategoryService {
+    Category getCategoryById(Long id);
+    List<Category> getCategoryByName(String name);
+    List<Category> getAllCategory();
+    Category createCategory(Category category);
+    void deleteCategory(Category category);
+    Category updateCategory(Long id, Category category);
 }
--- src/main/java/com/systems/backend/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/systems/backend/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.systems.backend.service.impl;
 
+import com.systems.backend.model.Category;
 import com.systems.backend.repository.CategoryRepository;
 import com.systems.backend.service.CategoryService;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +14,49 @@ import org.springframework.stereotype.Service;
 public class CategoryServiceImpl implements CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
+
+    @Override
+    public Category getCategoryById(Long id) {
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+        return categoryOptional.orElse(null);
+    }
+
+    @Override
+    public List<Category> getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    @Override
+    public List<Category> getAllCategory() {
+        return categoryRepository.findAll();
+    }
+
+    @Override
+    public Category createCategory(Category category) {
+        Category checkCategory = getCategoryById(category.getId());
+        if (checkCategory != null) {
+            throw new RuntimeException("This category is already existed");
+        }
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public void deleteCategory(Category category) {
+        Category checkCategory = getCategoryById(category.getId());
+        if (checkCategory == null) {
+            throw new RuntimeException("This category is not found");
+        }
+        categoryRepository.delete(category);
+    }
+
+    @Override
+    public Category updateCategory(Long id, Category category) {
+        Category updatedCategory = getCategoryById(id);
+        if (updatedCategory == null) {
+            throw new RuntimeException("This category is not found");
+        }
+        updatedCategory.setName(category.getName());
+        updatedCategory.setDescription(category.getDescription());
+        return categoryRepository.save(updatedCategory);
+    }
 }
