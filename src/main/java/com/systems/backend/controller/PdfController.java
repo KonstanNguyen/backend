ebd550aae8e70134be5c653bdccb6503ba72408b
Phi Long <nguyenphilong.it.123@gmail.com>
2024-12-07 12:50:24 +0700

    pdf download in pdf controller

--- src/main/java/com/systems/backend/controller/PdfController.java
+++ src/main/java/com/systems/backend/controller/PdfController.java
@@ -1,8 +1,14 @@
 package com.systems.backend.controller;
 
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +23,6 @@ public class PdfController {
             @RequestParam String pdfPath,
             @RequestParam int pageNumber) {
         try {
-            // Log cho việc debug
             System.out.println("pdfPath: " + pdfPath);
             System.out.println("pageNumber: " + pageNumber);
 
@@ -32,5 +37,20 @@ public class PdfController {
             e.printStackTrace();
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+    }
+
+    @GetMapping("/pdf/download")
+    public ResponseEntity<FileSystemResource> downloadPdf(@RequestParam String pdfPath) throws IOException {
+        String uploadDir = System.getProperty("user.dir") + File.separator + "uploads";
+
+        File file = new File(uploadDir + File.separator + pdfPath);
+        if (!file.exists()) {
+            return ResponseEntity.notFound().build();
+        }
+        FileSystemResource resource = new FileSystemResource(file);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + file.getName())
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(resource);
     }
 }
