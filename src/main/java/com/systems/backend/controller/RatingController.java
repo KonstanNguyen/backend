33eeba2f6ee2993e2d3abc60f4b089012fececbb
Phi Long <nguyenphilong.it.123@gmail.com>
2024-12-22 17:04:56 +0700

    add rattings

--- src/main/java/com/systems/backend/controller/RatingController.java
+++ src/main/java/com/systems/backend/controller/RatingController.java
@@ -18,7 +18,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("api/ratings")
@@ -39,4 +40,14 @@ public class RatingController {
     public RatingResponse createRating(@Valid @RequestBody CreateRatingRequest ratingRequest) {
         return ratingService.createRating(ratingRequest);
     } 
+
+    @GetMapping("{accountId}")
+    public ResponseEntity<List<RatingResponse>> getRatingByAccountId(@PathVariable Long accountId) {
+        try {
+            List<RatingResponse> rating = ratingService.getRatingByAccountId(accountId);
+            return ResponseEntity.ok(rating);
+        } catch (Exception ex) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
 }
